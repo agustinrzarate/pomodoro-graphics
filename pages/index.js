7c26977cd5b8b0d15befd1094abc9c24db550da2
agustin rafael zarate <agustinrzarate@gmail.com>
2022-04-05 11:03:29 -0300

    Create app

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h1">Hello world</Typography>
+    </Container>
+  );
+}
